c7e5bc9ca15a5a56c1ff1fe91a5d9929b378f45a
taniguchi <taniguchi.takumi01@gmail.com>
2022-08-30 16:15:52 +0900

    モーターリセット追加

--- workspace/src/Steering/RightMotor.cpp
+++ workspace/src/Steering/RightMotor.cpp
@@ -6,6 +6,7 @@ RightMotor::RightMotor()
 {
     portNum = EV3_PORT_B;
     ev3_motor_config( static_cast<motor_port_t>(portNum), LARGE_MOTOR);
+    ev3_motor_reset_counts(static_cast<motor_port_t>(portNum));
 }
 
 RightMotor::~RightMotor()
